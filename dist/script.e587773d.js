5930c65ea278ac32074e1f26180f4078589a13b6
Deon Jordaan <hello@deon.dev>
2022-06-04 13:06:32 +0200

    Working on spacing.

--- dist/script.e587773d.js
+++ dist/script.e587773d.js
@@ -207,7 +207,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59361" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64002" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
